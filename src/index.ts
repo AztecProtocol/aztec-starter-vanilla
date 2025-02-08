8d1107d8219ca173234f16c5466dbac25a766b29
Charlie Lye <karl.lye@gmail.com>
2025-02-08 18:13:32 +0000

    Release commit-a6882bc4f5efc75888f0af602b1abf41cc7e48b4.

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import { createPXEClient, AccountManager, Fr, Wallet, deriveMasterIncomingViewingSecretKey } from '@aztec/aztec.js';
+
+import { SingleKeyAccountContract } from '@aztec/accounts/single_key';
+import { VanillaContract } from '../artifacts/Vanilla';
+
+const setWait = (state: boolean): void =>
+  document.querySelectorAll('*').forEach((e: HTMLElement & HTMLButtonElement) => {
+    e.style.cursor = state ? 'wait' : 'default';
+    e.disabled = state;
+  });
+
+let contract: any = null;
+let wallet: Wallet | null = null;
+let account: AccountManager = null;
+
+document.querySelector('#deploy').addEventListener('click', async ({ target }: any) => {
+  setWait(true);
+  wallet = await account.register();
+  contract = await VanillaContract.deploy(wallet, Fr.random(), wallet.getCompleteAddress().address)
+    .send({ contractAddressSalt: Fr.random() })
+    .deployed();
+  alert(`Contract deployed at ${contract.address}`);
+
+  target.hidden = true;
+  document.querySelectorAll('#get, #set').forEach((e: HTMLButtonElement & HTMLFormElement) => (e.hidden = false));
+  setWait(false);
+});
+
+document.querySelector('#set').addEventListener('submit', async (e: Event) => {
+  e.preventDefault();
+  setWait(true);
+
+  const { value } = document.querySelector('#number') as HTMLInputElement;
+  const { address: owner } = wallet.getCompleteAddress();
+  await contract.methods.setNumber(parseInt(value), owner).send().wait();
+
+  setWait(false);
+  alert('Number set!');
+});
+
+document.querySelector('#get').addEventListener('click', async () => {
+  const viewTxReceipt = await contract.methods.getNumber(wallet.getCompleteAddress().address).simulate();
+  alert(`Number is: ${viewTxReceipt.value}`);
+});
+
+const secretKey = Fr.random();
+const pxe = createPXEClient(process.env.PXE_URL || 'http://localhost:8080');
+
+const encryptionPrivateKey = deriveMasterIncomingViewingSecretKey(secretKey);
+account = await AccountManager.create(pxe, secretKey, new SingleKeyAccountContract(encryptionPrivateKey));
+(document.querySelector('#deploy') as HTMLButtonElement).hidden = false;
